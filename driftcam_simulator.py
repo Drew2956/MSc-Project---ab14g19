22e6e2c5603788508c8f8f0dc32485ebadc61e52
Jose Cappelletto <cappelletto@gmail.com>
2019-02-07 16:23:27 +0000

    Mayor fix:  - Improve transect interpolation down to 10 cm. Current setup of 1.0m proved to be too coarse for fine control  - Added complete transect section in configuration.yaml  - Fine tuned controller from Z-N seed. However it must be noted that ZN tends to overshoot, so we ended with a scaled down version with more emphasis in the derivative part  - Add low pass filter for transect data to avoid spikes in the seafloor profile  - Removed unused print section  - Improved verbose output for simulation setup

--- driftcam_simulator.py
+++ driftcam_simulator.py
@@ -29,8 +29,9 @@ density_table = pd.read_csv(configuration['density_profile'], sep='\t', header=N
 # By doing this, the interpolation is done via lookup table, starting from 0 meter index
 
 # Starting values for the simulation
-depth = configuration['input']['start_depth']  
-vertical_velocity = configuration['input']['start_velocity']   
+depth = configuration['input']['start_depth']  # initial simulation depth
+vertical_velocity = configuration['input']['start_velocity']   # initial vertical velocity. For faithfull simulations, it should be close to 0.0
+horizontal_position = configuration['transect']['horizontal_position_offset']   # starting point in X axis for the transect mode simulations
 t = 0.0
 dropped_ball_time = 0   # timestamp of last dropped ball event
 
@@ -44,31 +45,44 @@ simulation_time = configuration['input']['simulation_time']  # End of simulation
 min_dispensing_time = configuration['input']['min_dispensing_time'] # minimum admissible time between drop ball event
 
 # define platform specific parameters
-gravity = configuration['input']['gravity']
-drag_coefficient = configuration['input']['drag_coefficient']
-radius = configuration['input']['radius']
-area = (math.pi*radius**2)
+gravity = configuration['input']['gravity'] # this could be compensated according to the latitude and the geodesic model
+drag_coefficient = configuration['input']['drag_coefficient']   # geometry defined drag coefficient of the platform
+radius = configuration['input']['radius']   # platform radius cross-section radius
+area = (math.pi*radius**2)  # cross-section area
 tau = configuration['input']['tau'] # system dynamic response constant
 
 main_mass = configuration['input']['main_mass'] # kg
 main_volume = configuration['input']['main_volume'] # m3
 
-ball_diameter = configuration['input']['ball_diameter']
-ball_volume = 4/3*math.pi*math.pow(ball_diameter/2,3)
+ball_diameter = configuration['input']['ball_diameter'] # ballast unit diameter
+ball_volume = 4/3*math.pi*math.pow(ball_diameter/2,3)   # ballast unit volume
 ball_density = configuration['input']['ball_density'] # kg / m3 # ball_mass = 0.016728 # kg 
-ball_mass = ball_volume * ball_density
+ball_mass = ball_volume * ball_density  # ballast unit mass
 
 flotation_mass = configuration['input']['flotation_mass'] # kg
 flotation_density = configuration['input']['flotation_density'] # kg/m3
-flotation_volume = flotation_mass / flotation_density
+flotation_volume = flotation_mass / flotation_density   # total volume of the flotation mass (assumed to be constant)
 
-ballast_mass = configuration['input']['ballast_mass']
-number_of_balls = math.ceil(ballast_mass / ball_mass)
+ballast_mass = configuration['input']['ballast_mass']   # design defined initial ballast_mass
+number_of_balls = math.ceil(ballast_mass / ball_mass)   # rounded-up number of ballast units based on the desired ballast_mass and the ballas unit mass
 
-# TODO: floor can be a fixed value or a transect profile
+# TODO: floor can be a fixed value or a transect profile (See #4)
 braking_altitude = configuration['control']['braking_altitude']
-floor_profundity = configuration['control']['floor_profundity']
 mission_duration = configuration['control']['mission_duration']
+
+floor_model = configuration['floor_model']  # retrieve floor model: fixed or transect
+if floor_model == 'fixed':
+    print ("Using 'floor_model': ", floor_model)
+    floor_profundity = configuration['control']['floor_profundity']
+elif floor_model == 'transect':
+    print ("Importing transect profile from: ", configuration['transect']['transect_profile'])
+    profundity_table = pd.read_csv(configuration['transect']['transect_profile'], sep='\t', header=None)
+    floor_profundity = profundity_table[1][0] + configuration['transect']['transect_profundity_offset']
+else:
+    print ("Unknown 'floor_model' ", floor_model, " defined in 'configuration.yaml'")
+    print ("Using default 'fixed' depth model with depth = 100m")
+    floor_model = 'fixed'
+    floor_profundity = 100
 
 #######################################
 # Print summary of simulation parameters
@@ -94,14 +108,21 @@ print ("\tTime step[s]: ", time_step, "\tGravity[m/s2]: ", gravity, "\tDrag coef
 density_id = configuration['density_profile'].split("_")[0]
 density_id = density_id.split("/")[-1]
 
-floor_id = str(floor_profundity)
+if floor_model == 'fixed':
+    floor_id = str(floor_profundity)
+elif floor_model == 'transect':
+    floor_id = configuration['transect']['transect_profile'].split("_")[0]
+    floor_id = floor_id.split("/")[-1]
 
 simulation_details = "_e" + str(eta) + "_d" + str(ball_diameter) + "_c" + str(density_id) + "_t" + floor_id
 
 #  Empty placeholders for incoming simulation data
 time_dive_history = []
 velocity_dive_history = []
+horizontal_position_history = []
+floor_profundity_history = []
 depth_dive_history = []
+error_history = []
 altitude_history = []
 net_buoyancy_dive_history = []
 drag_dive_history = []
@@ -117,28 +138,56 @@ print ("\nStarting DIVING mode")
 print ("Time: ", t, "\tDepth: ", depth)
 mode = 'DIVING'
 
-thruster_force = 0
-safety_factor_altitude = 1.0
-target_altitude = configuration['control']['target_altitude']
+thruster_force = 0  # starting with the thrusters OFF
+safety_factor_altitude = 1.0    # safety margin to trigger phase transition from BRAKING to CONTROL (basically increases the Htarget for the control, so it fires the thrusters before reaching the target)
+target_altitude = configuration['control']['target_altitude']   # reference value for the altitude controller
 
-# Setting up the PID controller
-_ku = 20   # Ultimate gain (Ziegler-Nichols): 300
-_Tu = 2.1   # Oscillation period (Ziegler-Nichols): 2.1 s
-#_kp = _ku / 1.7
-#_ki = _Tu / 2
-#_kd = _Tu / 8
-_kp = 2.3
-_ki = 0.02
-_kd = 4.5
+_kp = configuration['control']['kp_gain']
+_ki = configuration['control']['ki_gain']
+_kd = configuration['control']['kd_gain']
 
+# creates SimplePID controller object, with defined gains and hard limits for the control output
 pid = SimplePID(target_altitude, -100, 100, _kp, _ki, _kd, time_step * 1000 )
 
 keep_running = True
 
 #while t < simulation_time:     # limit simulation time and depth (due to speed constraints)
-while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_running == True):     # limit simulation time and depth (due to speed constraints)
+#while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_running == True):     # limit simulation time and depth (due to speed constraints)
+while (t < simulation_time) and (keep_running == True):     # limit simulation time and depth (due to speed constraints)
 
-    # replace weight with mass
+    if (depth > (floor_profundity + 10)):
+        print ("\n>>>>>>>>>\tPlatform crashed!!!")
+        print ("\n>>>>>>>>>\tHalting simulation...")
+        print (floor_profundity)
+        print (depth)
+        keep_running = False
+        break
+    ################################################
+    # HORIZONTAL MOVEMENT SIMULATION
+    # Only required for floor_model = 'transect' mode
+    ################################################
+    horizontal_position = horizontal_position + configuration['transect']['horizontal_speed']*time_step
+
+    ################################################
+    # FLOOR MODEL UPDATE
+    # This will affect floor_profundity depending on the current seafloor model
+    ################################################
+    # if current model is transect, update according to the transect table
+    if floor_model == 'transect':
+        # the new profundity value will be the profile value for the current horizontal position + the profundity offset
+        # TODO: try/catch if we exceed the maximum transect length
+        # WARNING: cheap and dirty low pass filter to avoid abrupt references changes
+        # that will trigger spikes in the controller
+        last_floor_profundity = floor_profundity
+        new_floor_profundity = profundity_table[1][math.floor(horizontal_position*10)] + configuration['transect']['transect_profundity_offset']
+
+        floor_profundity = 0.005*new_floor_profundity + 0.995*last_floor_profundity
+    # otherwise, we keep using the same initial value
+
+    ################################################
+    # MASS AND BUOYANCY UPDATE
+    ################################################
+    # replace weight with Mass
     microballast_mass = ball_mass * number_of_balls # kg
     microballast_volume = ball_volume * number_of_balls # m3
 
@@ -147,14 +196,14 @@ while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_runn
     total_mass = main_mass + microballast_mass + flotation_mass
     total_weight = total_mass*gravity
 
-    ####################
     # See if it is possible to improve speed while doing the density search in the input vector (already ordered)
     seawater_density = density_table[1][int (math.floor(depth))]
 
     ball_buoyancy = ball_volume*gravity*seawater_density    # weight of the displaced seawater volume (per ball)
 
-    ####################################
+    ################################################
     ## BODY-FLUID INTERACTION SIMULATION
+    ################################################
     buoyancy_force = total_volume*gravity*seawater_density
     drag_force = 0.5*drag_coefficient*seawater_density*area*abs(vertical_velocity)*vertical_velocity
     # calculate the actual net force experienced by the platform
@@ -172,8 +221,9 @@ while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_runn
         keep_running = False
         break
 
-    ####################################
+    ################################################
     ## FUTURE VELOCITY ESTIMATOR
+    ################################################
     future_velocity = vertical_velocity + 2*tau*(vertical_velocity-last_velocity)
 
     # Here we check if we need to dispense a ball, the conditions are:
@@ -184,6 +234,7 @@ while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_runn
 
     # TODO: estimate power consumption for each dispensing action (depth invariant, just need to multiply by the expected power consumed per ball drop action)
     current_altitude = floor_profundity - depth
+    altitude_error = 0  # this is and ampty value to fill the data vector when mode is NOT CONTROL
 
     # ------------------------------------------------------------------------------------------------------
     # DIVING MODE: default starting mode
@@ -221,12 +272,14 @@ while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_runn
     # CONTROL MODE: active altitude control with the thruster
     elif mode == 'CONTROL':
         altitude_error =  current_altitude - target_altitude
-        # print (altitude_error)
         #thruster_force = -_kp*altitude_error 
         # if abs(altitude_error) > 0.1:
         # print ("Controlling...........")
+        
+        last_thruster_force = thruster_force
         pid_out = pid.get_output_value(current_altitude)
-        thruster_force = pid_out
+
+        thruster_force = 0.2*pid_out + 0.8*last_thruster_force
 
         # we could check if the ellapsed time in CONTROL phase is higher than our mission time
         time_controlling = t - start_control_time
@@ -246,19 +299,10 @@ while (depth < (floor_profundity + 10)) and (t < simulation_time) and (keep_runn
                 # drop a single ball
                 number_of_balls -= 1
 
-    # print('-------------------------------')
-    # print('Time {0}'.format(t))
-    # print('Density {:4f}'.format(seawater_density))
-    # print('# Balls {0}'.format(number_of_balls))
-    # print('Weight: {:4f}'.format(total_weight))
-    # print('Buoyancy: {:4f}'.format(buoyancy_force))
-    # print('Drag: {:5f}'.format(drag_force))
-    # print('Net force: {}'.format(net_force))
-    # print('Acceleration: {}'.format(acceleration))
-    # print('Velocity: {:5f}'.format(vertical_velocity))
-    # print('Depth: {:2f}'.format(depth))
-
+    error_history.append(altitude_error)
     acceleration_dive_history.append(acceleration)
+    horizontal_position_history.append(horizontal_position)
+    floor_profundity_history.append(floor_profundity)
     time_dive_history.append((time_step+t))
     velocity_dive_history.append(vertical_velocity)
     depth_dive_history.append(depth)
@@ -284,22 +328,30 @@ output_df = pd.DataFrame(
 output_file = configuration['output']['file_path'] + configuration['output']['file_preffix'] + simulation_details + configuration['output']['file_extension']
 output_file_html = configuration['output']['file_path'] + "plot" + simulation_details + ".html"
 
-output_df.to_csv(output_file, encoding='utf-8', index=False)
+# WARNING: removed to speed up simulation process
+#output_df.to_csv(output_file, encoding='utf-8', index=False)
 
 ####################################################################
 # Creates plots using Plotly
+print ("Creating plots ...")
+
 trace1 = go.Scatter(y=depth_dive_history, x=time_dive_history, name='Depth')
 trace2 = go.Scatter(y=velocity_dive_history, x=time_dive_history, name='Velocity')
 trace3 = go.Scatter(y=thruster_history, x=time_dive_history, name='Thruster')
 trace4 = go.Scatter(y=balls_history, x=time_dive_history, name='Balls')
-
-data = [trace1, trace2, trace3, trace4]
+trace5 = go.Scatter(y=floor_profundity_history, x=time_dive_history, name='Profundity')
+trace6 = go.Scatter(y=error_history, x=time_dive_history, name='Altitude error')
 
 fig = tools.make_subplots(rows=2, cols=2, subplot_titles=('Depth', 'Velocity',
                                                           'Thruster', 'Balls'))
 fig.append_trace(trace1, 1, 1)
+fig.append_trace(trace5, 1, 1)
+
 fig.append_trace(trace2, 1, 2)
+
 fig.append_trace(trace3, 2, 1)
+fig.append_trace(trace6, 2, 1)
+
 fig.append_trace(trace4, 2, 2)
 
 fig['layout']['xaxis1'].update(title='Time (s)')
@@ -307,12 +359,13 @@ fig['layout']['xaxis2'].update(title='Time (s)')
 fig['layout']['xaxis3'].update(title='Time (s)')
 fig['layout']['xaxis4'].update(title='Time (s)')
 
-fig['layout']['yaxis1'].update(title='Depth (m)', autorange='reversed')
+fig['layout']['yaxis1'].update(title='Depth (m) & Profundity (m)', autorange='reversed')
 fig['layout']['yaxis2'].update(title='Velocity (m/s)')
-fig['layout']['yaxis3'].update(title='Thruster (N)')
+fig['layout']['yaxis3'].update(title='Thruster (N) & Altitude error (m)')
 fig['layout']['yaxis4'].update(title='Number of balls (#)')
 
 fig['layout'].update(title='Customizing Subplot Axes')
 
-print ("Done, now plotting figures")
+print ("Done, now storing plots in ", output_file_html)
+print ("This may take a while ...")
 py.plot(fig, filename = output_file_html)
--- transect_convert.m
+++ transect_convert.m
@@ -7,25 +7,30 @@ function transect_convert (filename, x_resolution)
 	% X[km] LAT[°] LON[°] Depth[m]  
 	printf ("done\n")
 	% First, we trim the last row, as contains repeated value for X (discontinuity problem for interpolation)
-	N = length(tmp(:,1)) - 1;
+	N = length(tmp(:,1)) - 4;
 	tmp = tmp(1:N,:);
 
 	% Then, we rearrange the data to a standard format:
 	% X[km] Depth[m] LAT[°] LON[°] 
-	data(1,:) = tmp (1,:);	% X position   
-	data(2,:) = tmp (4,:);   % Depth
-%	data(3,:) = tmp (2,:)   % Latitude
-%	data(4,:) = tmp (3,:)   % Longitude
+	x = tmp (:,1) * 112000;	% X position is given in 1:1000m
+	y = -tmp (:,2);   % Depth is given as negative value
+
+	plot (x,y)
+	hold on
 
 	% Now, we proceed to interpolate with the new resolution
-	printf ("%d rows to be processed\n", N)
-	xmax = max (data(1,:));
-	xf = [0:x_resolution:xmax];
-	yf = interp1 (data(1,:), data(2,:), xf, "pchip");	% spline tend to overshoot 
+	printf ("%d rows read from the input file\n", N)
+	xmax = max (x)
+	xf = [0:x_resolution:xmax]';
+	yf = interp1 (x, y, xf, "pchip");	% spline tend to overshoot 
+	% figure
+	plot (xf, yf, 'r')
 
+	N = length(xf(:,1));
+	printf ("%d rows exported to the output file\n", N)
 	printf ("Exporting interpolated data...\n")
-	out(1,:) = xf;
-	out(2,:) = yf;
+	out(:,1) = xf;
+	out(:,2) = yf;
 
 	% Finally, we export the new data
 	% TODO: trim extension
