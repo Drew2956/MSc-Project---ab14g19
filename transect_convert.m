92dcb345473b7be6e09262b0c273755aca3cafae
Jose Cappelletto <cappelletto@gmail.com>
2019-02-08 11:24:53 +0000

    Fix filename exported when interpolating with Octave/Matlab. Add 3x interpolated transects, with X resolution of 1 cm, and scale to 10.000 m transect length

--- transect_convert.m
+++ transect_convert.m
@@ -1,28 +1,28 @@
 % Import data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function transect_convert (filename, x_resolution)
+function transect_convert (filename, x_resolution, x_scale, y_scale = -1)
 	printf ("Loading %s ...", filename)
 	tmp = load (filename);
 	% Expected data format, from QGID 3.2 Profile plugin
-	% X[km] LAT[°] LON[°] Depth[m]  
+	% X[km] Depth[m]  Current Falkor data (and probably all incoming data uses negative depth values 
 	printf ("done\n")
 	% First, we trim the last row, as contains repeated value for X (discontinuity problem for interpolation)
-	N = length(tmp(:,1)) - 4;
+	N = length(tmp(:,1)) - 1;
 	tmp = tmp(1:N,:);
 
-	% Then, we rearrange the data to a standard format:
-	% X[km] Depth[m] LAT[°] LON[°] 
-	x = tmp (:,1) ;	% X position is given in 1:1000m
-	y = -tmp (:,2);   % Depth is given as negative value
+	% Then, we rearrange, scale and invert the data (if required) to our standard format:
+	% X[km] Depth[m]
+	xi = x_scale * tmp (:,1) ;  % X position is given in 1:100.000
+	yi = y_scale * tmp (:,2);   % Depth is given as negative value. Using default y_scale = -1 will invert depth to positive
 
-	plot (x,y)
+	plot (xi,yi)
 	hold on
 
 	% Now, we proceed to interpolate with the new resolution
 	printf ("%d rows read from the input file\n", N)
-	xmax = max (x)
+	xmax = max (xi)
 	xf = [0:x_resolution:xmax]';
-	yf = interp1 (x, y, xf, "spline");	% spline tend to overshoot 
+	yf = interp1 (xi, yi, xf, "spline");	% spline tend to overshoot. Alternative are pchip and linear 
 	% figure
 	plot (xf, yf, 'r')
 
@@ -33,7 +33,8 @@ function transect_convert (filename, x_resolution)
 	out(:,2) = yf;
 
 	% Finally, we export the new data
-	% TODO: trim extension
-	dlmwrite(strcat (filename, "_interpolated.csv"), out, 'delimiter', '\t');
-	
-% Export data
+	[fPath fName fExtension] = fileparts (filename)
+	new_filename = strcat(fPath,"/",fName,"_interpolated.csv")
+
+	dlmwrite(new_filename, out, 'delimiter', '\t');
+
