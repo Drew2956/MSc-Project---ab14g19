d8cfdbe780a05ad80c2c060a206d8ff47f3c9ec5
Jose Cappelletto <cappelletto@gmail.com>
2019-02-09 10:16:16 +0000

    Test 01: Summary table results and Matlab batch processing script created

--- simulation_time_estimator.m
+++ simulation_time_estimator.m
@@ -1,27 +1,41 @@
 % Import data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function simulation_time_estimator (filename)
-	printf ("Loading %s ...", filename)
-	data = dlmread (filename, ',', 1,0);
-	% Expected data format, from QGID 3.2 Profile plugin
-	% X[km] Depth[m]  Current Falkor data (and probably all incoming data uses negative depth values 
-	printf ("done\n")
-	% First, we trim the last row, as contains repeated value for X (discontinuity problem for interpolation)
-	N = length(data(:,1));
+function simulation_time_estimator (file_list)
 
-	time = data(:,7);
-	depth = data(:,4);
-	thruster = data(:,6);
+	% Look for all CSV files in the provided folder root (recursively)
+	nfiles = size(file_list)(1);
+	printf ("Total number of files: %d\n", nfiles)
 
-	[val idx] = max (thruster);
+	for i = 1:nfiles
+		% Need to trim a trailing whitespace in the end
+		filename = strtrim(file_list(i,:));
+		l = length(filename);
+		filename = filename(1:l);
 
-	diving_time = time(idx);
-	surfacing_time = time(N) - diving_time;
-	printf ("Diving time: %f\n", diving_time)
-	printf ("Surfacing time: %f\n", surfacing_time)
-	printf ("\t Total time: %f\n", time(N))
+		[fPath fName fExtension] = fileparts (filename);
+		printf ("Loading %s ...", fName)
+		data = dlmread (filename, ',', 1,0);
+		% Expected data format, from QGID 3.2 Profile plugin
+		% X[km] Depth[m]  Current Falkor data (and probably all incoming data uses negative depth values 
+		printf ("done\n")
+		% First, we trim the last row, as contains repeated value for X (discontinuity problem for interpolation)
+		N = length(data(:,1));
 
-%	new_filename = strcat(fPath,"/",fName,"_interpolated.csv")
+		time = data(:,7);
+		depth = data(:,4);
+		thruster = data(:,6);
 
-%	dlmwrite(new_filename, out, 'delimiter', '\t');
+		[val idx] = max (thruster);
+
+		diving_time(i) = time(idx);
+		surfacing_time(i) = time(N) - diving_time(i);
+		printf ("Diving time: %f\n", diving_time(i))
+		printf ("Surfacing time: %f\n", surfacing_time(i))
+		printf ("\t Total time: %f\n", time(N))
+	
+	endfor
+
+	new_filename = strcat(fPath,"/","diving_time_list.csv")
+    out = [diving_time' surfacing_time']
+	dlmwrite(new_filename, out, 'delimiter', '\t');
 
