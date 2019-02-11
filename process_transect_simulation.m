f3a1402232ac41bc1536c4831897a25815adbc17
Jose Cappelletto <cappelletto@gmail.com>
2019-02-11 12:08:57 +0000

    Closes #7, closes #9 (actually fixed few commits ago)

--- process_transect_simulation.m
+++ process_transect_simulation.m
@@ -26,13 +26,45 @@ function process_transect_simulation (filename, row_skip = 0)
 
     N = size (data)(1)
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Simulation wise parameters
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
     time_step = 0.1         % seconds: dT solver
     control_time = 36000    % 10 hrs = 36.000 seconds in CONTROL mode
     target_altitude = 2.0   % target altitude employed for ALL the current simulation set
+
+    altitude = depth - prof;
+    altitude_error = altitude - target_altitude;
+
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % THRUSTER MODEL:
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %---For M100 with 3D printed propeller
+    nominal_force = 1.1;     % [kg.f] Force of the thruster at the nominal operation point (usually, the most efficient point)
+    nominal_force = nominal_force * 9.81;    % Convert from [kg.f] to [N]
+    nominal_power = 80;     % [W] Power consumed when the thruster is at the nominal operation point
+
+    %---For T100 thruster
+    t100_factor = 42.637;    % [g.f / W] highest efficiency point for the T100 according to datasheet
+    t100_factor = t100_factor * 9.81 / 1000; % convert from grams-force to Newton
+
+    force_to_power = t100_factor;  % use the model factor. This can be changed according to the thruster employed
+
+    power_required = abs(force) * force_to_power;     % instant power from the thruster to provide the required force 
+
+    figure
+    subplot (3,1,1)
+    plot (x, power_required, 'linewidth', 2)
+    hold on
+    plot (x, force, 'k--')
+
+    subplot (3,1,2)
+    plot (x, prof)
+
+    subplot (3,1,3)
+    plot (x, altitude)
+
 
 	printf ("%d entries loaded...\n", N)
 
@@ -49,22 +81,12 @@ function process_transect_simulation (filename, row_skip = 0)
     % We have both the actual depth, and the instant velocity for each (t)
     % We should be able to estimate the distance traveled (dZ) for each interval (dT)
     % The time step is constant: 0.1 s for the existing dataset
+    % WARNING: this calculations won't hold true as the hypothesis required is to be a conservative system
+    % which is not our case, as there are dissipative forces (drag, i.e.)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    power_t = force .* velocity;     % Power = Force x Velocity
-    power_avg = mean (abs(power_t)); % Average power
-
-    total_power = sum(abs(power_t)) * time_step;
-
-    energy_t = force .* velocity * time_step; % Energy = Force x Distance = Force x Velocity x Time
-    total_energy = sum(abs(energy_t)) * time_step; % Total Energy = integral (Energy . dt)
-
-    plot (power_t)
-    hold on 
-    plot (energy_t, 'r')
-
     return
-    altitude = depth - prof;
 
+ 
     close all
 %    figure
 %	hold on
@@ -72,25 +94,19 @@ function process_transect_simulation (filename, row_skip = 0)
 %    plot (x, prof + target_altitude, 'g--')
 %    plot (x, depth)
 %    grid on
-
-    figure
+    plot (altitude, 'r--')
     hold on
-    plot (x, energy, 'r')
-    %plot (x, target_altitude*ones(N,1), 'g')
-    plot (x, thruster, 'k')
-    grid on
+    plot (force, 'k')
     return
 
 
     % TODO: determine the system mode: DIVING, BRAKING, CONTROL, SURFACING
-    % TODO: create issue in dritcam_simulator.py to add a vector column with information regarding the platform operatin mode
+    % TODO: create issue in dritcam_simulator.py to add a vector column with information regarding the platform operating mode
     % so this doesn't require postprocessing in this section of the pipeline
 
     % In order to detect the PHASE, we can use as input mission_time: 36.000 for CONTROL phase,
     % the start of ballast dispensing fror init BRAKING phase, and target_altitude for BRAKING->CONTROL
     % Also, the second ballast init, flags the SURFACING phase
-
-
 
 	% Finally, we export the new data
 	[fPath fName fExtension] = fileparts (filename)
